143da77ddf1cad878245412a307b2defef2ae036
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-08-01 18:10:08 -0500

    Delete square_or_cube_first.hs

--- square_or_cube_first.hs
+++
@@ -1,7 +0,0 @@
-sq list = [x * x | x <- list]
-cb list = [x * x * x | x <- list]
-
-sclist :: [Integer] -> [Integer]
-
-sclist jj = if odd (head jj) then (cb jj) ++ (sq jj)
-	   else (sq jj) ++ (cb jj)
